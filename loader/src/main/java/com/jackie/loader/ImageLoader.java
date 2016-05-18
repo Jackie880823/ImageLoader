4dd4555a8a89cf0091d908879ee1e127d6c54dea
Jackie <jackie880823@gmail.com>
2016-05-18 22:49:19 +0800

    Feature: 简单实现下载图片并缓存到内存中,并显示在ImageView中

---
+++ loader/src/main/java/com/jackie/loader/ImageLoader.java
@@ -0,0 +1,120 @@
+/*
+ *    Copyright 2016 The Open Source Project of Jackie Zhu
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ *             $                                                   $
+ *             $                       _oo0oo_                     $
+ *             $                      o8888888o                    $
+ *             $                      88" . "88                    $
+ *             $                      (| -_- |)                    $
+ *             $                      0\  =  /0                    $
+ *             $                    ___/`-_-'\___                  $
+ *             $                  .' \\|     |$ '.                 $
+ *             $                 / \\|||  :  |||$ \                $
+ *             $                / _||||| -:- |||||- \              $
+ *             $               |   | \\\  -  $/ |   |              $
+ *             $               | \_|  ''\- -/''  |_/ |             $
+ *             $               \  .-\__  '-'  ___/-. /             $
+ *             $             ___'. .'  /-_._-\  `. .'___           $
+ *             $          ."" '<  `.___\_<|>_/___.' >' "".         $
+ *             $         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       $
+ *             $         \  \ `_.   \_ __\ /__ _/   .-` /  /       $
+ *             $     =====`-.____`.___ \_____/___.-`___.-'=====    $
+ *             $                       `=-_-='                     $
+ *             $     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   $
+ *             $                                                   $
+ *             $          Buddha bless         Never BUG           $
+ *             $                                                   $
+ *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ */
+
+package com.jackie.loader;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 图片加载类
+ * Created by on 16/5/18.
+ *
+ * @author Jackie Zhu
+ * @version 1.0
+ */
+public class ImageLoader {
+    // 图片缓存
+    LruCache<String, Bitmap> mImageCache;
+    // 线程池,线程数量为CPU的数量
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime()
+            .availableProcessors());
+
+    public ImageLoader() {
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        // 计算可用的最大内存
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        // 取四分之一的可用内存作为缓存
+        int cacheSize = maxMemory / 4;
+
+        mImageCache = new LruCache<String, Bitmap>(cacheSize){
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getRowBytes()* value.getHeight() / 1024;
+            }
+        };
+    }
+
+    public void displayImage(final String url, final ImageView imageView) {
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null) {
+                    return;
+                }
+                if (url.equals(imageView.getTag())) {
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url, bitmap);
+            }
+        });
+    }
+
+    private Bitmap downloadImage(String imageUrl) {
+        Bitmap result = null;
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            result = BitmapFactory.decodeStream(conn.getInputStream());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
