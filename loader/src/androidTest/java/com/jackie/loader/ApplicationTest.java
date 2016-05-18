807aef458f80b50f831a7948ffb04c3c45d00ee9
Jackie <jackie880823@gmail.com>
2016-05-18 22:48:52 +0800

    Add: 添加Loader在这个Module用于编写对图片加载的实现库

--- loader/src/androidTest/java/com/jackie/loader/ApplicationTest.java
+++ loader/src/androidTest/java/com/jackie/loader/ApplicationTest.java
@@ -40,4 +40,16 @@
  *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
  */
 
-include ':app', ':loader'
+package com.jackie.loader;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
